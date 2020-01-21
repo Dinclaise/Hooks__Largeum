36e7eb12324b61641b13e6f69815365520410508
Anton <aanohin98@gmail.com>
2020-01-21 16:45:00 +0300

    0.3.0
    //In Progress
    in folder src have a folder is node_modules for absolute path

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Routes from 'routes';
+import TopBar from 'components/topBar';
+
+
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <TopBar/>
+                <Routes/>
+            </Router>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
